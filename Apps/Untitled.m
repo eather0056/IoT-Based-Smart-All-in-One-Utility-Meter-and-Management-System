d1833e9656aaec48f61a57d12616769879fd913f
Md Ether Deowan <62349621+eather0056@users.noreply.github.com>
2024-01-22 01:26:31 +0100

    Add files via upload

---
+++ Apps/Untitled.m
@@ -0,0 +1,8 @@
+%thingSpeakWrite(1679858,8,'Writekey','5EK39WRFRTY5I7AA',"Fields",7)
+%thingSpeakWrite(1679858,2,'Writekey','5EK39WRFRTY5I7AA',"Fields",5)
+% thingSpeakWrite(1679858,3,'Writekey','5EK39WRFRTY5I7AA',"Fields",3)
+% thingSpeakWrite(1679858,4,'Writekey','5EK39WRFRTY5I7AA',"Fields",4)
+% thingSpeakWrite(1679858,5,'Writekey','5EK39WRFRTY5I7AA',"Fields",5)
+% thingSpeakWrite(1679858,6,'Writekey','5EK39WRFRTY5I7AA',"Fields",6)
+% thingSpeakWrite(1679858,7,'Writekey','5EK39WRFRTY5I7AA',"Fields",7)
+% thingSpeakWrite(1679858,8,'Writekey','5EK39WRFRTY5I7AA',"Fields",8)
